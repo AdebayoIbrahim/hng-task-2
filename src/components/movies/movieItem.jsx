e11bf997be9ce8ad4dc63635dcf3020afe84bbd2
SetUp <abdulmumeenibroheem51@gmail.com>
2023-09-13 00:13:40 +0100

    getting array fetch result length

---
+++ src/components/movies/movieItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Movieitem = () => {
+  return <div>Movieitem</div>;
+};
+
+export default Movieitem;
--- src/components/movies/movielist.jsx
+++ src/components/movies/movielist.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 const Movielists = () => {
+  const [resultarr, setResultarr] = useState();
   const [load, setLoad] = useState(true);
   const api_key = "d2af3cef5640d578a1839a201a48a671";
   const api_url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}`;
@@ -12,12 +13,18 @@ const Movielists = () => {
       };
       const movieList = await fetch(`${api_url}`, options);
       const result = await movieList.json();
-      console.log(result);
+      const dataEnd = result.results;
+      setResultarr(dataEnd);
       setLoad(false);
     };
     fetchData();
   }, []);
-  return load ? <h3>Loading...</h3> : <h2>Result gotten!!!!</h2>;
+
+  return load ? (
+    <h3>Loading...</h3>
+  ) : (
+    <h2>Result gotten!!!!{resultarr.length}</h2>
+  );
 };
 
 export default Movielists;
