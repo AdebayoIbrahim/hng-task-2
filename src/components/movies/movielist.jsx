4c5cc533464a28b674cbf093e6f09a7388e11654
SetUp <abdulmumeenibroheem51@gmail.com>
2023-09-13 00:01:10 +0100

    simple loading text

--- src/components/movies/movielist.jsx
+++ src/components/movies/movielist.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 const Movielists = () => {
+  const [load, setLoad] = useState(true);
   const api_key = "d2af3cef5640d578a1839a201a48a671";
-
   const api_url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}`;
   useEffect(() => {
     const fetchData = async () => {
@@ -13,10 +13,11 @@ const Movielists = () => {
       const movieList = await fetch(`${api_url}`, options);
       const result = await movieList.json();
       console.log(result);
+      setLoad(false);
     };
     fetchData();
   }, []);
-  return <Box>Movielists</Box>;
+  return load ? <h3>Loading...</h3> : <h2>Result gotten!!!!</h2>;
 };
 
 export default Movielists;
