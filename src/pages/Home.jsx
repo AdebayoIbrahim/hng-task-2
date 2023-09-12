9d377f88042ea5f68f98c174ad48565efc09ccd1
SetUp <abdulmumeenibroheem51@gmail.com>
2023-09-12 19:53:34 +0100

    navbar styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,34 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import Poster from "../assets/Poster.png";
+import Logo from "../assets/Logo.png";
 const Home = () => {
   return (
     <>
-      <Box></Box>
+      <Box
+        sx={{
+          background: `url(${Poster})`,
+          width: "100%",
+          minHeight: "90vh",
+          backgroundSize: "cover",
+          padding: "1.2rem 8%",
+        }}
+      >
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Box>
+            <img src={Logo} alt="logo" />
+          </Box>
+          <Box>Search..</Box>
+          <Stack direction="row" gap={2}>
+            <Typography variant="body2">Sign in</Typography>
+            <Typography variant="caption">Click</Typography>
+          </Stack>
+        </Stack>
+      </Box>
     </>
   );
 };
