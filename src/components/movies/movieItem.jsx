54a3c88fbae451cf7e524af377931dc971a0b6fe
SetUp <abdulmumeenibroheem51@gmail.com>
2023-09-13 01:20:11 +0100

    getting movies background_path

--- src/components/movies/movieItem.jsx
+++ src/components/movies/movieItem.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Grid, Box } from "@mui/material";
+import { Grid, Box, Stack } from "@mui/material";
 const Movieitem = ({ movies }) => {
+  const baseUrl = "https://image.tmdb.org/t/p/original";
   return (
     <>
       <Grid item xl={2} lg={3} md={4} sm={6} xs={12}>
         <Box>
-          <h2>{movies.original_title}</h2>
-          <h2>{movies.id}</h2>
+          <Box>
+            <img
+              src={`${baseUrl}${movies.backdrop_path}`}
+              alt="card-image"
+              style={{ width: "100%", height: "300px" }}
+            />
+          </Box>
+          <Stack></Stack>
         </Box>
       </Grid>
     </>
