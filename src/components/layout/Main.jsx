8333a50d5f5f16161ddb147810008064a540d049
SetUp <abdulmumeenibroheem51@gmail.com>
2023-09-13 22:15:56 +0100

    getting movie background url

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 import { useParams } from "react-router-dom";
 const Main = () => {
-  const [moviearr, setMoviearr] = useState([]);
+  const [moviearr, setMoviearr] = useState(null);
   const [spin, setSpin] = useState(true);
   const params = useParams();
-
+  //image url
+  const baseUrl = "https://image.tmdb.org/t/p/original";
   useEffect(() => {
     const api_key = "d2af3cef5640d578a1839a201a48a671";
     const api_url = `https://api.themoviedb.org/3/movie/${params.id}?api_key=${api_key}`;
+
     const fetchMovie = async () => {
       const options = {
         accept: "application/json",
@@ -23,13 +25,27 @@ const Main = () => {
       }
       const result = await movieData.json();
       console.log(result);
+      setSpin(false);
+      setMoviearr(result);
     };
     fetchMovie();
   }, []);
   return (
     <React.Fragment>
       <Box pt={8} pr={4}>
-        <Box sx={{ width: "100%", height: "60vh" }}></Box>
+        {spin ? (
+          <h2>Loading...</h2>
+        ) : (
+          <Box
+            sx={{
+              width: "100%",
+              height: "60vh",
+              background: `url(${baseUrl}${moviearr.backdrop_path})`,
+              backgroundSize: "cover",
+              borderRadius: "20px",
+            }}
+          ></Box>
+        )}
       </Box>
     </React.Fragment>
   );
