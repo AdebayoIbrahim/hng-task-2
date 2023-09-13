a347fb37da5e26ee0d2fe58bd4e3ef5fb468e180
SetUp <abdulmumeenibroheem51@gmail.com>
2023-09-13 14:34:17 +0100

    installed react-icons

--- src/pages/movie.jsx
+++ src/pages/movie.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Box, Stack } from "@mui/material";
 import { Link, useParams } from "react-router-dom";
+import Sidebar from "../components/layout/Sidebar";
 const Movie = () => {
   const params = useParams();
   return (
     <Box>
       <Stack direction="row" gap={2} sx={{ width: "100%" }}>
-        <Box flex="1.8" sx={{ background: "yellow", height: "100vh" }}></Box>
+        <Box flex="1.8" sx={{ background: "yellow", height: "100vh" }}>
+          <Sidebar />
+        </Box>
         <Box flex="7" sx={{ background: "red", height: "100vh" }}></Box>
       </Stack>
     </Box>
